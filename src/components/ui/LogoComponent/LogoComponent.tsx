6115b05f4b6d2d121c127f1d195b4b7febdfcc75
otegamike111 <otegamike111@gmail.com>
2026-06-06 09:21:26 +0100

    feat: add Logo component and styles for WelcomeVideo background layer

--- src/components/ui/LogoComponent/LogoComponent.tsx
+++ src/components/ui/LogoComponent/LogoComponent.tsx
@@ -2,12 +2,12 @@ import styles from "./logoComponent.module.css";
 
 export default function Logo() {
   return (
-    <div className={styles.logo_container}>
+    <a href="/" className={styles.logo_container}>
       <img src="/logo_small.png" alt="FarmCast" className={styles.logo} />
       <span className={styles.wordmark}>
         <span className={styles.wordmarkFarm}>Farm</span>
         <span className={styles.wordmarkCast}>Cast</span>
       </span>
-    </div>
+    </a>
   );
 }
